4e708baecc8667bba883008b27572d8500143dd6
Albertogc25 <113980825+Albertogc25@users.noreply.github.com>
2022-10-18 19:08:44 +0000

    Ejercicio1_Terminado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,4 +1,8 @@
 def ejercicio1():
-  lista = ["P", "t"]
+  lista = ["P", "t"] 
+  lista.append("h")
+  lista.append("o")
+  lista.append("n")
+  lista.insert(1, "y")
   print("lista = ", lista)
   assert "".join(lista) == "Python"
