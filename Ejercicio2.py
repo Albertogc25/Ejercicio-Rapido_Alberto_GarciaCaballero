a9b6a2f2999b305818144db87201b2913469c894
Albertogc25 <113980825+Albertogc25@users.noreply.github.com>
2022-10-18 19:17:53 +0000

    Ejercicio2_Terminado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,12 @@
 def ejercicio2():
     lista = [1, 4, 2, 5, 4, 3, 4, 7, 5, 8, 9]
-    # TODO
+    del lista[1]
+    del lista[2]
+    del lista[2]
+    del lista[2]
+    lista.remove("7")
+    lista.remove("8")
+    lista.remove("9")
+    lista.append("6")
+    print("lista = ", lista)
     assert lista == list(range(1, 6))
